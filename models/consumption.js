1903778f8040ef048b8df7e0d4d433e96d6aaa64
Ángel Torregrosa Domínguez <angtordom1@alum.us.es>
2022-03-17 23:59:33 +0100

    Feat: Unir backend con frontend para detalles de alimentos #49
    He añadido un nuevo archivo carrusel.js debido que al intentar usar /alimentos/carrusel en el frontend me redirigía a /alimentos/:id. He intentado solucionarlo pero la manera más fácil es separarlo y dado que a carrusel solo se accede para desde alimentos pues no importa mucho

---
+++ models/consumption.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const consumption = new Schema({
+    username: String,
+    product_id: String,
+    num_consumption: Number,
+    last_consumption: Date,
+    image_url: String,
+    energy_kcal_100g: Number,
+    fat_100g: Number,
+    carbohydrates_100g: Number,
+    proteins_100g: Number,
+});
+
+module.exports = mongoose.model('consumption', consumption);
--- routes/alimentos.js
+++ routes/alimentos.js
@@ -3,13 +3,12 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const alimento = require('../models/alimento');
-
-// Get con todos los documentos
+const Alimento = require('../models/alimento');
+var Consumicion = require('../models/consumption')
 
 router.get('/', async (req, res) => {
   try{
-    const alimentosDB = await alimento.find().limit(100);
+    const alimentosDB = await Alimento.find().limit(100);
     res.json(alimentosDB);
   } catch (error) {
     return res.status(400).json({
@@ -22,7 +21,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   const _id = req.params.id;
   try {
-    const alimentoDB = await alimento.findOne({_id});
+    const alimentoDB = await Alimento.findOne({_id});
     res.json(alimentoDB);
 } catch (error) {
     return res.status(400).json({
@@ -37,7 +36,7 @@ router.post('/', async(req, res) => {
   try {
       console.log("Creando un nuevo alimento", body);
       body._id = new mongoose.Types.ObjectId();
-      const alimentoDB = await alimento.create(body);
+      const alimentoDB = await Alimento.create(body);
       res.status(200).json(alimentoDB); 
   } catch (error) {
       console.log(error);
@@ -54,7 +53,7 @@ router.put('/:id', async(req, res) => {
   const body = req.body;  
   try {
       console.log("Actualizando un alimento")
-      const alimentoDB = await alimento.findByIdAndUpdate(_id, body);
+      const alimentoDB = await Alimento.findByIdAndUpdate(_id, body);
       res.status(200).json(alimentoDB);
   } catch (error) {
       return res.status(500).json({
@@ -67,13 +66,106 @@ router.put('/:id', async(req, res) => {
 router.delete('/:id', async(req, res) => {
   const _id = req.params.id;
   try {
-      const alimentoDB = await alimento.findByIdAndDelete(_id);
+      const alimentoDB = await Alimento.findByIdAndDelete(_id);
       res.status(200).json(alimentoDB);
   } catch (error) {
       return res.status(500).json({
           mensaje: 'Ha ocurrido un error',
           error
       })
+  }
+});
+
+router.get('/favoritos/:username', async (req, res) => {
+
+  var alimentos =[];
+  const lista_productos = [];
+  const name = req.params.username;
+  const items = await Consumicion.find({"username": name},{"product_id": 1, "_id":0}).sort({num_consummation: 1}).limit(500);
+  console.log(items);
+  if (items.length > 0){
+    for (var i = 0; i < items.length; i++){
+      lista_productos.push(items[i]["product_id"]);
+    }
+    alimentos = await Alimento.find({"_id":lista_productos}).limit(500);
+    
+  } else{
+    alimentos = await Alimento.find().limit(500);
+  }
+  
+  
+  res.json(alimentos);
+  
+});
+
+router.get('/recientes/:username', async (req, res) => {
+
+  var alimentos =[];
+  const lista_productos = [];
+  const name = req.params.username;
+  const items = await Consumicion.find({"username": name},{"product_id": 1, "_id":0}).sort({"last_consumption": -1}).limit(500);
+  console.log(items);
+  if (items.length > 0){
+    for (var i = 0; i < items.length; i++){
+      lista_productos.push(items[i]["product_id"]);
+    }
+    alimentos = await Alimento.find({"_id":lista_productos}).limit(500);
+  } else{
+    alimentos = await Alimento.find().limit(500);
+  }
+  
+  
+  res.json(alimentos);
+  
+});
+
+router.post('/newConsumption', async (req, res, next) => {
+  var item = new Consumicion(req.body);
+  item.num_consumption = 1;
+  item.last_consumption = Date();
+  const consumicion = await Consumicion.find({"username": item.username, "product_id": item.product_id});
+  if (consumicion.length == 0){
+      item.save()
+      .then(item => {
+        res.status(200).json({'item': 'Consumption added successfully'});
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  }else{
+    Consumicion.findOneAndUpdate({ "_id": consumicion[0]._id },{
+      $set: {
+        num_consumption: consumicion[0].num_consumption+1,
+        last_consumption: Date()
+    }
+  },
+  function(error, info) {
+    if (error) {
+        res.json({
+            resultado: false,
+            msg: 'No se pudo modificar la consumición',
+            error
+        });
+    } else {
+        res.json({
+            resultado: "Modificado con éxito",
+            info: info
+        })
+    }
+  }
+  )
+}
+});
+
+router.get('/carrusel/', async (req, res) => {
+  try{
+    const alimentosDB = await Alimento.find().limit(10);
+    res.json(alimentosDB);
+  } catch (error) {
+    return res.status(400).json({
+      mensaje: 'Ha ocurrido un error',
+      error
+    })
   }
 });
 
