e9991b44e3d3d2c715aa9ca01e68ab12dedf8d7b
Gonlalle <gonlalalv@alum.us.es>
2022-03-18 20:08:41 +0100

    Fix: arreglo url #47

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.get('/api/v1/logout', async(req, res) => {
 
 app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/users', require('./routes/users'));
-app.use('api/v1/comida', require('./routes/alimentos'));
+app.use('/api/v1/alimentos', require('./routes/alimentos'));
 
 module.exports = app;
