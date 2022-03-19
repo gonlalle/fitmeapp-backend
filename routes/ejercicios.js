0e3ec4a52e076400ee5759fd1c05f3bc2d895bc6
Beatriz Llamas <beallasai@alum.us.es>
2022-03-19 23:24:24 +0100

    Feat: randomizado ejercicios  recomendados #51

--- routes/ejercicios.js
+++ routes/ejercicios.js
@@ -5,7 +5,9 @@ var Ejercicio = require('../models/ejercicio');
 // Get con todos los documentos
 
 router.get('/', async (req, res) => {
-  const items = await Ejercicio.find().limit(8);
+  const items = await Ejercicio.find({  
+    $expr: { $lt: [0.8, {$rand: {} } ] }
+ }).limit(8);
   res.json(items);
 
 });
