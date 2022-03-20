179a9a1d47bcde40a8eb8acd03d0be410a450755
geniusbat-Tytan <43751753+geniusbat@users.noreply.github.com>
2022-03-20 13:29:49 +0100

    Feat: modelo materiales

---
+++ routes/material.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+// Require Item model in our routes module
+var Material = require('../models/material');
+
+// Get con todos los documentos
+
+router.get('/', async (req, res) => {
+    const items = await Material.find();
+    res.json(items);
+});
+
+router.get('/:id', async (req, res) => {
+    const _id = req.params.id;
+    try {
+        const items = await Material.find({_id});
+        res.json(items);
+    } catch (error) {
+        return res.status(400).json({
+        mensaje: 'An error has occurred',
+        error
+        })
+    }
+});
+
+router.get('/getnombres/:idString', async (req, res) => {
+    const ids = req.params.idString.split(",");
+    try {
+        ret = ""
+        for (idChar in ids) {
+            var _id = parseInt(idChar);
+            const items = await Material.find({_id});
+            ret += items["name"];
+        }
+        res.json(ret);
+    } catch (error) {
+        return res.status(400).json({
+        mensaje: 'An error has occurred',
+        error
+        })
+    }
+});
+
+// Exportamos la configuración de express app
+module.exports = router;
--- server.js
+++ server.js
@@ -33,4 +33,5 @@ app.get("/", (req, res) => {
 // });
 app.use('/api/v1/ejercicio', require('./routes/ejercicio'));
 app.use('/api/v1/musculo', require('./routes/musculo'));
+app.use('/api/v1/material', require('./routes/material'));
 module.exports = app;
