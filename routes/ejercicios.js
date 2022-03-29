f48cc26a13447affdc4df0749fcbd2c580484991
Beatriz Llamas <beallasai@alum.us.es>
2022-03-29 11:49:22 +0200

    Feat: vistas panel administración #116

--- routes/ejercicios.js
+++ routes/ejercicios.js
@@ -5,9 +5,7 @@ var Ejercicio = require('../models/ejercicio');
 // Get con todos los documentos
 
 router.get('/', async (req, res) => {
-  const items = await Ejercicio.find({  
-    $expr: { $lt: [0.8, {$rand: {} } ] }
- }).limit(8);
+  const items = await Ejercicio.find();
   res.json(items);
 
 });
