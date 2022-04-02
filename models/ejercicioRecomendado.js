4184f8e8e4213e14e1e2508d8fae72952e8dbbd8
DiegoHill <diegillos@alum.us.es>
2022-04-02 23:05:39 +0200

    Feat: Implementar ejercicios recomendados #173
    Modelo actualizado, colección en la base de datos creada.

--- models/ejercicioRecomendado.js
+++ models/ejercicioRecomendado.js
@@ -2,12 +2,25 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ejercicioRecomendado = new Schema({
-    _id: Number,
-    uuid: String,
-    ejercicio: Number,
-    hecho: Boolean,
-    intensidad: Number,
+    fecha: {
+        type: Date,
+        default: Date.now
+      },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    ejercicio: {
+        type: Schema.Types.ObjectId,
+        ref: 'Exercise'
+    },
+    intensidad: {
+        type: String,
+        enum : ["Baja","Media","Alta"],
+        default: "Media"
+      },
     tiempo: Number,
+    hecho: Boolean
 });
 
 module.exports = mongoose.model('ejerciciosRecomendados', ejercicioRecomendado);
