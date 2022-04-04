7b0990f914446ffeb58a50570fba3dcb220b3098
DiegoHill <diegillos@alum.us.es>
2022-04-04 22:35:59 +0200

    Fix: Reestructuración del backend de ejercicios #171
    Backend terminado.

--- server.js
+++ server.js
@@ -65,7 +65,6 @@ app.use('/api/v1/auth', require('./routes/auth'));
 app.use('/api/v1/users', require('./routes/users'));
 app.use('/api/v1/exercise', require('./routes/exercise'));
 app.use('/api/v1/ejercicios', require('./routes/ejercicios'));
-app.use('/api/v1/done_exercise', require('./routes/done_exercise'));
 app.use('/api/v1/alimentos', require('./routes/alimentos'));
 app.use('/api/v1/comidas', require('./routes/comidas'));
 
